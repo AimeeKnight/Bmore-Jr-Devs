5f017a9bf5694ccc57e9f64e1fc263216e3b33e3
AimeeKnight <aimee.knight@messagesystems.com>
2015-01-24 10:36:02 -0500

    set up basics for jade and views

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var express = require('express');
 var app = express();
 var pg = require('pg');
 
-app.set('port', (process.env.PORT || 5000));
+//app.set('port', (process.env.PORT || 5000));
+var port = process.env.PORT || 5000;
 
 var morgan = require('morgan'); // log requests to the console (express4)
 var bodyParser = require('body-parser'); // pull information from HTML POST (express4)
@@ -15,23 +16,37 @@ app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.set('views', __dirname + '/public');
+app.set('view engine', 'jade');
+
+app.use(function(req, res, next) {
+  console.log(req.method, req.url);
+  next();
 });
 
-app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.send(result.rows); }
+app.route('/')
+  .get(function(request, response) {
+    response.render('index2');
+    //response.send('Hello World!');
+  });
+
+app.route('/db')
+  .get(function (request, response) {
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+      client.query('SELECT * FROM test_table', function(err, result) {
+        done();
+        if (err) {
+          console.error(err); response.send("Error " + err);
+        } else {
+          response.send(result.rows);
+        }
+      });
     });
   });
-})
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
+app.listen(port, function() {
+  console.log("Node app is running at localhost:" + port);
 });
+
+module.exports = app;
 
